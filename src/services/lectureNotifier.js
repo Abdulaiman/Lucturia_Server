8e90762697d9a2d729019df96307f129455bb15b
Abdulazeez Rabiu <abduldanmaihaja@gmail.com>
2025-09-29 14:44:23 +0100

    change time to 2 47

--- src/services/lectureNotifier.js
+++ src/services/lectureNotifier.js
@@ -5,7 +5,7 @@ const dayjs = require("dayjs"); // optional, for date manipulation
 
 // Schedule daily at 8 PM
 const lectureNotifierJob = cron.schedule(
-  "33 14 * * *", // 7:30 PM daily
+  "47 14 * * *", // 7:30 PM daily
   async () => {
     console.log("📤 Running lecture notification job...");
 
