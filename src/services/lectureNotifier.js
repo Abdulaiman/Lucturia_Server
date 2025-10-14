53791706f585fd4786f65824170ebcf3072654d9
Abdulazeez Rabiu <abduldanmaihaja@gmail.com>
2025-10-14 17:55:46 +0100

    fixed all bugs

--- src/services/lectureNotifier.js
+++ src/services/lectureNotifier.js
@@ -10,7 +10,7 @@ dayjs.extend(timezone);
 
 // Schedule daily at 7:30 PM Lagos time
 const lectureNotifierJob = cron.schedule(
-  "20 17 * * *", // 19:30 = 7:30 PM
+  "00 20 * * *", // 19:30 = 7:30 PM
   async () => {
     console.log("📤 Running lecture notification job...");
 
